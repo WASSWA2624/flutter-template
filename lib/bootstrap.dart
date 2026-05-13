91d6a014d4e05fa828968dfde114f7b69d990477
Wilson Wasswa <wasswawilson0002@gmail.com>
2026-05-13 12:30:15 +0300

    Integrate Riverpod for state management by wrapping TemplateApp in ProviderScope during bootstrap.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/app/app.dart';
 
 void bootstrap() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const TemplateApp());
+  runApp(const ProviderScope(child: TemplateApp()));
 }
